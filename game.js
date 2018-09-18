9731d950a77221123179bed326c9e2ce25180fe7
ptorrsmith <peter@ptorrsmith.com>
2018-09-18 15:39:30 +1200

    Outline goals, add in click goes blue

--- game.js
+++ game.js
@@ -3,27 +3,67 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
+
+/*
+
+Objective:
+  * right click a dot it goes green
+  * left click a dot it goes blue
+  * double-click and it will hide, or become visible again (assume with same colour?)
+  * * dots should not lose their space, just become invisible (but still clickable)
+  * the colour counters keep count of their dots
+
+*/
+
 function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+  
+  // find all the elements with class=="board"
+  // select the first one [0]
+  // return the collection of child elements, being all the dot divs 0-8
+  // then call the bindEventListeners function with the collection as input   
+   bindEventListeners(document.getElementsByClassName('board')[0].children)
 }
 
+// receive the collection of elements as dots[]
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+
+    // 'contextmenu' is the righ-click mouse event
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
+//    dots[i].addEventListener('dblclick', hide);
   }
 }
 
 function makeGreen (evt) {
+  // override / stop the default right-click mouse event behaviours (the context menu)
   evt.preventDefault()
-  evt.target.classList.toggle('green')
+  evt.target.classList.toggle('green');
+  // evt.target.classList.remove('blue');
+  // evt.target.classList.remove('hide');
   updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+  // notice this doesn't care what event is received, as long as it has a target element property / object
+  evt.target.classList.toggle('blue');
+  // trigger count update
+  updateCounts();
+}
+
 // CREATE FUNCTION hide HERE
+
+// function hide (evt) {
+//   evt.target.classList.toggle('invisible');
+//   // trigger count update of invisible counter
+//   updateCounts();
+// }
+
+
 
 function updateCounts () {
   var totals = {
@@ -39,6 +79,7 @@ function updateCounts () {
 }
 
 function displayTotals (totals) {
+  // get key from totals Object
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
   }
